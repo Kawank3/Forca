ae821fa086c400ef621e76ba340b8e9b376a1735
Kawan Weber <kawan_weber@hotmail.com>
2025-03-23 11:48:53 -0300

    Forca

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+string palavraSecreta;
+string letra;
+string resultado;
+
+Console.Write("--- Forca ---\n");
+Console.Write("Qual a palavra secreta? ");
+palavraSecreta = Console.ReadLine()!.ToUpper()!;
+
+while (true)
+{
+    Console.Write("Qual a letra? ");
+    letra = Console.ReadLine()!.ToUpper();
+
+    if (letra.Length == 1)
+    {
+        resultado = palavraSecreta.Contains(letra) ? "Existe" : "Não existe";
+        Console.WriteLine($"A letra \"{letra}\" existe na palavra secreta? Resultado... {resultado}");
+
+        break;
+    }
+
+
+    else
+    {
+        Console.WriteLine("Digite somente uma letra");
+    }
+}
